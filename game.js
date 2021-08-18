6fb0d0a8cc9e65d30a17e9cdcfaa55a3fc441b93
coderlore <coderlore@gmail.com>
2021-08-18 08:00:42 -1000

    Add restart and handleClick functions in module

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const boardContainer = document.querySelectorAll('board');
 
-const GameBoard = (function() {
+/* const GameBoard = (function() {
     let player = 'X';
     const board = ['','','','','','','','',''];
 
@@ -18,22 +18,55 @@ const GameBoard = (function() {
         //board = ['','','','','','','','',''];
         alert("Game Restarted!")
     }
-    return {restartGame}
+    return { restartGame }
+    let _restart = function(){
+        alert('Boom Boom')
+    }
+
+    return { restart: _restart}
 
     function handleClick(event) {
         const clickedCellNumber = event.getAttribute('data-btn')
         console.log(clickedCellNumber);
     }
-    return { handleClick }
+    return {handleClick} 
+    
+
+})(); */
+
+//document.getElementById('cell_0').addEventListener('click', GameBoard.handleClick);
+
+//This works, but need to get this into the module
+/* document.querySelectorAll('.cell').forEach(cell => {
+    cell.addEventListener('click', handleClicked, { once: true})
+})
+
+function handleClicked(e) {
+    console.log('Clicked!')
+} */
+
+//document.getElementById('restart').addEventListener('click', GameBoard.restartGame);
+
+const gameBoard = (function(){
+    let board = ['','','','','','','','',''];
+
+    let _restart = function(){
+        alert('I restarted')
+        board = ['','','','','','','','',''];
+    }
+    document.getElementById('restart').addEventListener('click', _restart)
+
+    //return {restart: _restart}
+
+    let _handleClicky = function(e){
+        let square = e.target.id;
+        console.log(square)
+    }
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.addEventListener('click', _handleClicky, { once: true})
+    })
 })();
 
-const Player = (name, symbol) => {
-    const hasTurn = true;
-    const getName = () => name;
-    const getSymbol = () => symbol;
+//gameBoard.restart();
+//document.getElementById('restart').addEventListener('click',gameBoard.restart)
 
-    return { hasTurn, getName, getSymbol };
-};
-
-document.querySelectorAll('.cell').forEach(button => button.addEventListener('click', GameBoard.handleClick));
-document.getElementById('restart').addEventListener('click', GameBoard.restartGame);
